bea7e6d8ae173af74364ec8836a8cbee0694f31f
Joanna Shevchuk <42650695+joashev@users.noreply.github.com>
2018-11-22 16:50:08 +0200

    Updated the TOC module

--- slangtoc.py
+++ slangtoc.py
@@ -1,7 +1,16 @@
-from string import ascii_lowercase
+#!/usr/bin/env python3
 
-slangpages = []
-for c in ascii_lowercase:
-	slang = "http://www.peevish.co.uk/slang/" + c + ".htm"
-	slangpages.append(slang)
+def slang_dict_toc(): 
+	from string import ascii_lowercase
+
+	slangpages = []
+
+	for c in ascii_lowercase:
+		slang = "http://www.peevish.co.uk/slang/" + c + ".htm"
+		slangpages.append(slang)
+
+	return slangpages
 	
+if __name__ == "__main__":
+	slangpages = slang_dict_toc()
+	print(slangpages)
